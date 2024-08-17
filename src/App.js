0d4e48ab160e81b16367785faac12d304c0d3b7a
Kohei Miyamoto <91676145+koheitech@users.noreply.github.com>
2024-08-17 10:50:13 +0900

    refactor: use `of` for the array loop

--- src/App.js
+++ src/App.js
@@ -93,8 +93,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+  for (let [a, b, c] of lines) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
