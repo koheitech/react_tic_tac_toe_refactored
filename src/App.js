c33c395a3a9a13be6cafea9b894328d396f0d263
Kohei Miyamoto <91676145+koheitech@users.noreply.github.com>
2024-08-17 10:48:11 +0900

    refactor: use const

--- src/App.js
+++ src/App.js
@@ -63,19 +63,16 @@ export default function Game() {
     setCurrentMove(nextMove);
   }
 
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
+  const moves = history.map((_, move) => {
+    const description = move > 0 ? `Go to move #${move}` : 'Go to game start';
+
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
   });
+  
 
   return (
     <div className="game">
